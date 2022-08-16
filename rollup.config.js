61e2382c1a745adb031f1af3302bee2063e2c118
gianlucaguarini <gianluca.guarini@gmail.com>
2022-08-16 21:27:42 +0200

    updated: dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,7 @@ import resolve from 'rollup-plugin-node-resolve'
 export default {
   input: 'index.next.js',
   plugins: [
-    resolve({
-      jsnext: true
-    })
+    resolve()
   ],
   output: [
     {
