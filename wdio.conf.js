8ddf3adfd3eec30c57e2c4420d2d4fbd0272ed72
Gianluca Guarini <gianluca.guarini@gmail.com>
2024-11-02 23:40:38 +0100

    updated: fix the wdio options

--- wdio.conf.js
+++ wdio.conf.js
@@ -49,8 +49,13 @@ export const config = {
   //
   capabilities: [
     {
-      // capabilities for local browser web tests
-      browserName: 'chrome', // or "firefox", "microsoftedge", "safari"
+      maxInstances: 5,
+      browserName: 'chrome',
+      acceptInsecureCerts: true,
+      // We need to extends some Chrome flags in order to tell Chrome to run headless
+      'goog:chromeOptions': {
+        args: ['--headless', '--disable-gpu', '--disable-dev-shm-usage'],
+      },
     },
   ],
 
